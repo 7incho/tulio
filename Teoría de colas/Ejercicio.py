bfedc40a111f0f6c6e269326e0410378b7554867
Alexis Joel Bustos Gutierrez <alexisbustos1809@gmail.com>
2023-05-19 05:11:36 -0300

    Primera Función Simulación

--- Teoría de colas/Ejercicio.py
+++ Teoría de colas/Ejercicio.py
@@ -1,18 +1,14 @@
 import random
 
-Q, PS, T, P, SigLlegada, SigFinServicio, SigLlegadaS, SigSalidaS, tci, tcf, tai, taf, caso, S, tdi, tdf, tti, ttf = 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+Q, PS, T, P, SigLlegada, SigFinServicio, SigLlegadaS, SigSalidaS, tci, tcf, tai, taf, caso, S, tdi, tdf, tti, ttf, header = 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
 
 def VectorInicial():
-    global Q, PS, T, P, SigLlegada, SigFinServicio, SigLlegadaS, SigSalidaS, tci, tcf, tai, taf, caso, S, tdi, tdf, tti, ttf
+    global Q, PS, T, P, tci, tcf, tai, taf, caso, S, tdi, tdf, tti, ttf, header
 
     Q = int(input("Ingrese la cantidad inicial de clientes en cola: "))
     PS = int(input("Ingrese el estado inicial del puesto de trabajo (ocupado = 1; libre = 0): "))
     T = int(input("Ingrese la duración de la simulación (en minutos): "))
     P = 0
-    SigLlegada = 0
-    SigFinServicio = 0
-    SigLlegadaS = 0
-    SigSalidaS = 0
     
     print("Ingrese el intervalo en el que llegan los clientes: ") 
     tci = int(input("Mínimo: "))
@@ -32,30 +28,30 @@ def VectorInicial():
         print("Ingrese el intervalo de tiempo en el que trabaja el servidor: ")
         tti = int(input("Mínimo: "))
         ttf = int(input("Máximo: "))
+        header = ["Hora actual", "H.Prox llegada cliente", "H.Prox fin servicio", "H.Prox Salida servidor", "H.Prox Llegada servidor", "Q", "PS", "S"]
+    else:
+        S = 1
+        header = ["Hora actual", "H.Prox llegada cliente", "H.Prox fin servicio", "Q", "PS"]
 
 def LlegadaCliente():
     global Q, PS, P, SigFinServicio
-
-    a = random.randint(tci, tcf)
-    if PS == 0:
+    if PS == 0 and S == 1:
         PS = 1
-        SigFinServicio = P + a
+        SigFinServicio = P + random.randint(tai, taf)
     else:
         Q = Q + 1
+    SigLlegada = SigLlegada + random.randint(tci, tcf)
 
 def FinServicio():
-    global Q, PS
-
-    a = random.randint(tai, taf)
+    global Q, PS, SigFinServicio
     if Q >= 1:
         Q = Q - 1
-        SigFinServicio = P + a
+        SigFinServicio = P + random.randint(tai, taf)
     else:
         PS = 0
 
 def SalidaServidor():
     global S, SigLlegadaS, SigFinServicio
-
     a = random.randint(tdi, tdf)
     S = 0
     SigLlegadaS = P + a
@@ -64,10 +60,27 @@ def SalidaServidor():
 
 def LlegadaServidor():
     global S, SigSalidaS
-
-    a = random.randint(tti, ttf)
     S = 1
-    SigSalidaS = P + a
+    SigSalidaS = P + random.randint(tti, ttf)
+    
+def Simulacion():
+    global P, PS, T, Q, SigFinServicio, SigLlegada
+    print("-------------- Inicio de Simulación --------------")
+    print(header)
+    tabla = [P, SigLlegada, SigFinServicio, Q, PS] 
+    LlegadaCliente
+    while True:
+        print(tabla)
+        P = min(SigFinServicio, SigLlegada)
+        
+        if min(SigFinServicio, SigLlegada) == SigFinServicio:
+            FinServicio
+        elif min(SigFinServicio, SigLlegada) == SigLlegada:
+            LlegadaCliente
+        if P >= T:
+            print("-------------- Fin de la simulación -------------- ") 
+            break           
+    return
 
 def ImprimirVector():
     print("Clientes en cola:", Q)
@@ -89,7 +102,8 @@ def ImprimirEventos():
 # Llamadas a las funciones
 
 VectorInicial()
-ImprimirVector()
+Simulacion()
+# ImprimirVector()
 # LlegadaCliente()
 # FinServicio()
 # SalidaServidor()
