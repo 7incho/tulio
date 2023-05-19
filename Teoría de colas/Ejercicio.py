6eb0a264ea32725f19513a3b5eac6b39f76f766c
Nacho386 <nachosilva388@gmail.com>
2023-05-19 02:47:31 -0300

    prueba1

--- Teoría de colas/Ejercicio.py
+++ Teoría de colas/Ejercicio.py
@@ -1,10 +1,36 @@
+import datetime as dt
 
+ps = 0
+q = 0
+horaActual = dt.timedelta(hours=8)
+deltaLLegadas = 45
+deltaFS = 40
+horaProximoFinServicio = 0 
 
 def VectorInicial():
     return
 
 def LlegadaCliente():
-    return
+    global ps, q, horaActual, horaProximaLlegada, deltaLLegadas
+   
+    if ps==0:
+        ps=1
+    else:
+        q+=1
+    #generar horario de proxima llegada
+    horaProximaLlegada = horaActual + dt.timedelta(seconds=deltaLLegadas)
+         
+    print('ps: ',ps, ' q:',q, 'Hora Actual: ', horaActual, 'Proxima llegada: ', horaProximaLlegada)
+   
 
 def FinServicio():
+    global ps, q, deltaFS, horaActual, horaProximoFinServicio, deltaFS
+    if q>0:
+        ps=1    #esta linea es opcional (por que si es un fin de servicio entonces ps==1)
+        q=q-1
+        #calcular proximo fin de servicio
+        horaProximoFinServicio = horaActual + dt.timedelta(seconds=deltaFS)
+    else:  
+        ps=0
+    print('ps: ', ps, ' q:', q, 'Hora Actual: ', horaActual, 'Proxima llegada: ', horaProximaLlegada, 'Hora prox. fin servicio: ', horaProximoFinServicio)
     return
